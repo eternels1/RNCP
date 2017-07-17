e7e6b3548d07e710f165d4ef7bb3457ca2160640
khalid <abaroukhalid@hotmail.com>
2017-07-17 10:29:43 +0200

    17/07/2017matin

---
+++ codeJava/javaReflection/src/javaReflection/gui/Fenetre.java
@@ -0,0 +1,14 @@
+package javaReflection.gui;
+
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class Fenetre extends JFrame {
+
+	public Fenetre(String title) throws HeadlessException {
+		super("reflectir");
+		// TODO Auto-generated constructor stub
+	}
+
+}
